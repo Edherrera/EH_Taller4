73ee8dd64f200c3c98e6c7027ada72076ad683ec
unknown <eduardo>
2020-09-27 17:03:30 -0500

    Factorial de N

---
+++ factorial.py
@@ -0,0 +1,12 @@
+def factorial(x,n):
+ if(n>0):
+  x=factorial(x,n-1)
+  x=x*n
+ else:
+  x=1
+ return x
+n=int(input("ingresa un numero:  "))
+x=1
+x=factorial(x,n)
+#print "El factorial de ", n ,"es: ",x
+print (x)
